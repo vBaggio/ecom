80c682aafdf6f47d7435aeb95b8c2434a1e9fbef
vbaggio <vnc.baggio@gmail.com>
2025-05-28 23:55:08 -0300

    added: entity OrderItem

--- src/main/java/com/vbaggio/ecom/entitites/Order.java
+++ src/main/java/com/vbaggio/ecom/entitites/Order.java
@@ -1,6 +1,9 @@
 package com.vbaggio.ecom.entitites;
 
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -10,6 +13,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
@@ -32,6 +36,9 @@ public class Order {
 	
 	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
 	private Payment payment;
+	
+	@OneToMany(mappedBy = "id.order", cascade = CascadeType.ALL)
+	private List<OrderItem> items = new ArrayList<>();
 
 	public Order() {
 		super();
@@ -84,6 +91,31 @@ public class Order {
 
 	public void setPayment(Payment payment) {
 		this.payment = payment;
+	}
+
+	public List<OrderItem> getItems() {
+		return items;
+	}
+	
+	public List<Product> getProducts(){
+		return items.stream().map(x -> x.getProduct()).toList();
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		return Objects.equals(id, other.id);
 	}
 	
 }
--- src/main/java/com/vbaggio/ecom/entitites/Product.java
+++ src/main/java/com/vbaggio/ecom/entitites/Product.java
@@ -1,6 +1,9 @@
 package com.vbaggio.ecom.entitites;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 import jakarta.persistence.Column;
@@ -11,6 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -35,6 +39,9 @@ public class Product {
 		joinColumns = @JoinColumn(name = "product_id"), 
 		inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
+	
+	@OneToMany(mappedBy = "id.product")
+	private List<OrderItem> items = new ArrayList<>();
 
 	public Product() {
 		super();
@@ -91,6 +98,31 @@ public class Product {
 
 	public Set<Category> getCategories() {
 		return categories;
+	}
+
+	public List<OrderItem> getItems() {
+		return items;
+	}
+	
+	public List<Order> getOrders() {
+		return items.stream().map(x -> x.getOrder()).toList();
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(id, other.id);
 	}
 	
 }
