df424c729ee4be8db22be97a713874f213e5b4dd
vbaggio <vnc.baggio@gmail.com>
2025-06-03 21:50:11 -0300

    updated: Product - search by name

--- src/main/java/com/vbaggio/ecom/controllers/ProductController.java
+++ src/main/java/com/vbaggio/ecom/controllers/ProductController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -34,8 +35,8 @@ public class ProductController {
 	}
 
 	@GetMapping
-	public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) {
-		return ResponseEntity.ok(service.findAll(pageable));
+	public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable, @RequestParam(defaultValue = "") String name) {
+		return ResponseEntity.ok(service.findAll(name, pageable));
 	}
 
 	@PostMapping
--- src/main/java/com/vbaggio/ecom/repositories/ProductRepository.java
+++ src/main/java/com/vbaggio/ecom/repositories/ProductRepository.java
@@ -1,8 +1,14 @@
 package com.vbaggio.ecom.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.vbaggio.ecom.entitites.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long>{
+	
+	@Query("SELECT p FROM Product p WHERE UPPER(p.name) LIKE UPPER(CONCAT('%', :name, '%'))")
+	Page<Product> searchByName(String name, Pageable pageable);
 }
